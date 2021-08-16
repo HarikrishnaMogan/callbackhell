582cb3a602bcedb205b3681475c2788e69e07fa7
HarikrishnaMogan <harikrishss437@gmail.com>
2021-08-16 13:31:21 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,40 @@
+let heading = document.createElement("h1");
+heading.innerText="";
+heading.className="heading";
+document.body.append(heading);
+setTimeout(()=>{
+    heading.innerText="10";
+    setTimeout(()=>{
+        heading.innerText="9";
+        setTimeout(()=>{
+            heading.innerText="8";
+            setTimeout(()=>{
+                heading.innerText="7";
+                setTimeout(()=>{
+                    heading.innerText="6";
+                    setTimeout(()=>{
+                        heading.innerText="5";
+                        setTimeout(()=>{
+                            heading.innerText="4";
+                            setTimeout(()=>{
+                                heading.innerText="3";
+                                setTimeout(()=>{
+                                    heading.innerText="2";
+                                    setTimeout(()=>{
+                                        heading.innerText="1";
+                                        setTimeout(()=>{
+                                            heading.innerText="0";
+                                            setTimeout(()=>{
+                                                heading.innerText="HAPPY INDEPENDENCE DAY !!!!";
+                                            },1000)
+                                        },1000)
+                                    },1000)
+                                },1000)
+                            },1000)
+                        },1000)
+                    },1000)
+                },1000)
+            },1000)
+        },1000)
+    },1000)
+},1000)
